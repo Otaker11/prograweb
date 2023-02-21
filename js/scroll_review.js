69ed704fb9e6633ecc8e571fbc255393e788aea6
Otaker11 <84486353+Otaker11@users.noreply.github.com>
2023-02-21 13:31:14 -0600

    Actualizacion V3
    Ajustes en css y javascript

--- js/scroll_review.js
+++ js/scroll_review.js
@@ -1,10 +1,13 @@
 const sr = ScrollReveal({
-    distance: '60px',
-    duration: 2800,
-    // reset: true,
+  distance: "60px",
+  duration: 2800,
+  // reset: true,
 });
 
-sr.reveal(".redondo, .location-image, .col-md-8, .col-md-4, .col-md-6, .col-2, .col-4, .col-6, .col-8, .col-md-10, .card, .footer-left, .footer-center, .footer-right, .blanco-index, .negro-title, .blanco, .blanco-salud",{
-    origin: 'top',
+sr.reveal(
+  ".redondo, .location-image, .col-md-8, .col-md-4, .col-md-6, .col-2, .col-4, .col-6, .col-8, .col-md-10, .card, .footer-left, .footer-center, .footer-right, .blanco-index, .negro-title, .blanco, .blanco-salud",
+  {
+    origin: "top",
     interval: 100,
-});
+  }
+);
